a2acd64f0f840b9eff63278bfb8c8cef07057092
James Owen <jamestheski@gmail.com>
2026-05-12 16:32:57 -0700

    add stuff

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,3 @@
-
 local builtin = require('telescope.builtin')
 
 -- Use Telescope to find files
@@ -7,10 +6,16 @@ vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 -- Use Telescope to search through git files
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
+local last_search = ""
 -- Use Telescope to grep string
 vim.keymap.set('n', '<leader>ps', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    last_search = vim.fn.input("Grep > ")
+    builtin.grep_string({ search = last_search })
 end, { noremap = true })
+
+vim.keymap.set('n', '<leader>pa', function()
+    builtin.grep_string({ search = last_search })
+end, {noremap = true})
 
 vim.keymap.set('n', '<leader>pd', builtin.lsp_definitions, {desc = "Go to Definition"})
 vim.keymap.set('n', '<leader>pr', builtin.lsp_references, {desc = "Go to References"})
